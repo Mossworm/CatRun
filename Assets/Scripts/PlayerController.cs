a811f681c9e3d0455518072e92d7fa3c4a0fa840
yuchanahn <37448462+yuchanahn@users.noreply.github.com>
2022-05-27 21:44:23 +0900

    플레이어 이동코드 약간 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,6 +16,8 @@ public class PlayerController : MonoBehaviour
 
     public Vector3 postion;
     [SerializeField] float Speed = 10;
+    char dir;
+    static Dictionary<KeyCode, char> input_map = new Dictionary<KeyCode, char>();
 
     void Start()
     {
@@ -23,35 +26,46 @@ public class PlayerController : MonoBehaviour
         postion = transform.position;
 
         hpBar = Instantiate(prfHPBar,canvas.transform).GetComponent<RectTransform>();
+
+        input_map[KeyCode.LeftArrow] = (char)1;
+        input_map[KeyCode.RightArrow] = (char)2;
+        input_map[KeyCode.UpArrow] = (char)10;
+        input_map[KeyCode.DownArrow] = (char)20;
     }
+
+    void movement_input(char dir_) {
+        dir = dir_;
+    }
+
+    void movement() {
+        var r = dir % 10;
+
+        if (r == 1) {
+            this.GetComponent<SpriteRenderer>().flipX = false;
+            postion.x -= Speed * Time.deltaTime;
+        }
+        else if (r == 2) {
+            this.GetComponent<SpriteRenderer>().flipX = true;
+            postion.x += Speed * Time.deltaTime;
+        }
+        if ((dir >= 10) && (dir < 20)) {
+            postion.y += Speed * Time.deltaTime;
+        }
+        else if (dir >= 20) {
+            postion.y -= Speed * Time.deltaTime;
+        }
+        transform.position = postion;
+    }
+
 
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            this.GetComponent<SpriteRenderer>().flipX = false;
-            postion.x -= Speed*Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            this.GetComponent<SpriteRenderer>().flipX = true;
-            postion.x += Speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            postion.y += Speed*Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            postion.y -= Speed * Time.deltaTime;
-        }
-        transform.position = postion;
+        movement_input((char)input_map.Select(x => Input.GetKey(x.Key) ? x.Value : 0).Sum());
 
+        movement();
         Vector3 _hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
         hpBar.position = _hpBarPos;
-
-
     }
 
 
